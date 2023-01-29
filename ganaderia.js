9733d8608e87fb952fa5006701c510ec1340cf6d
vato2003 <natanael1322134078@gmail.com>
2023-01-29 16:06:51 -0600

    Primera parte del trabajo

---
+++ ganaderia.js
@@ -0,0 +1,17 @@
+class Ganaderia {
+    constructor (Alimento,Estado,peso){
+        this._Alimento=Alimento;
+        this._Estado = Estado;
+        this._Peso = peso;
+    }
+
+
+
+}
+
+const vobina = new Ganaderia('pasto','Bien',350);
+
+console.log(vobina._Alimento);
+console.log(vobina._Estado);
+console.log(vobina._Peso);
+
